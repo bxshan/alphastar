d9dc14aaae5eed5a2157491cc5507dbc91b234a4
Boxuan.Shan <boxuan.shan@gmail.com>
2022-03-13 16:31:30 +0800

    added Moo times and Secret mission

---
+++ Moo_Times_main/times.cc
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int N;
+struct Game {
+	int g;
+	string n;
+	int p;	
+};
+
+bool comp(Game a, Game b) {
+	return a.g < b.g;
+}
+
+string comp_int(int b, int e, int m) {
+	if (b > e && b > m) {
+		return "b";
+	} else if (e > b && e > m) {
+		return "e";
+	} else if (m > b && m > e) {
+		return "m";
+	} else if (b == e && e == m) {
+		return "bem";
+	} else if (b == e) {
+		return "be";
+	} else if (b == m) {
+		return "bm";
+	} else if (e == m) {
+		return "em";
+	} else {
+		return "box";
+	}
+}
+
+int main() {
+   	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	cin >> N;
+	
+	vector<Game> g(N);
+   	for (Game& i: g) cin >> i.g >> i.n >> i.p;
+	sort(g.begin(), g.end(), comp);
+  	
+	int counter = 0;
+	string leader = "x";
+	int b = 0; int e = 0; int m = 0;	
+	//for (Game& i: g) cout << i.g << " " << i.n << " " << i.p << "\n";
+
+	// g - game, n - name, p - points
+	for (Game& i: g) {
+		if (i.n == "Bessie") {
+			b += i.p;
+		} else if (i.n == "Elsie") {
+			e += i.p;
+		} else if (i.n == "Mildred") {
+			m += i.p;
+		}
+
+		//cout << "========";
+		//cout << i.n << " == " << b << " == " << e << " == " << m << "\n";
+		
+		string new_leader = comp_int(b, e, m);
+		//cout << leader << " new === " << new_leader << "\n";
+		if (leader != new_leader) {
+			leader = new_leader;
+			counter++;
+		}
+	}
+
+	cout << counter << "\n";
+}	
---
+++ Secret_mission_main/secret_mission.cc
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string A, B;
+
+int getIndex(vector<char> alphabet, char c) {
+	 auto it = find(alphabet.begin(), alphabet.end(), c);
+	 int index = it - alphabet.begin();
+	 return index;
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	cin >> A >> B;
+	string alphabetStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	vector<char> alphabet(alphabetStr.begin(), alphabetStr.end());
+	
+	int anum = 1;
+	int bnum = 1;
+
+	for (char i: A) {
+		anum *= getIndex(alphabet, i) + 1;
+	}
+   	anum = anum % 47;
+
+	for (char j: B) {
+		bnum *= getIndex(alphabet, j) + 1;
+
+	//	cout << " index = " << index << " bnum = " << bnum << "\n";
+	}
+   	bnum = bnum % 47;
+
+	if (anum == bnum) {
+		cout << "GO";
+	} else {
+		cout << "STAY";
+	}
+}
