9aad56781476f216ac3a4586b6e8f942feb8e013
Boxuan.Shan <boxuan.shan@gmail.com>
2022-03-17 20:29:58 +0800

    added Acorn_Shuffle_main

---
+++ Acorn_Shuffle_main/acorn_shuffle.cc
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int N;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	
+	cin >> N;
+	vector<int> rule(N);
+	vector<int> id(N);
+	for (int& r: rule) cin >> r, r -= 1;
+	for (int& r: id) cin >> r;
+
+	vector<int> l[4];
+	for (vector<int>& i: l) i.reserve(N);
+
+	for (int r = 0; r < N; r++) l[0][r] = r + 1;
+	
+
+		
+	for (int i = 1; i < 4; i++) {
+		for (int j = 0; j < N; j++) {
+			l[i][j] = l[i-1][rule[j]];
+		}
+	}
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			if (l[3][j] - 1 == i) {
+				cout << id[l[3][i] - 1];
+				cout << endl;
+			}
+	   	}
+   	}
+}
