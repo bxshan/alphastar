85a94052f863256dc2fd6a7482c4d72bec2217a1
Boxuan.Shan <boxuan.shan@gmail.com>
2022-04-19 22:34:23 +0800

    Added Dream_main

---
+++ Dream_main/dream.cc
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int A, B;
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cin >> A >> B;
+	
+	vector<int> digits(10, 0);
+
+	for (int i = A; i <= B; i++) {
+		int number = i;
+		while (number > 0) { 
+			int digit = number % 10;
+		   	number /= 10;
+			digits[digit]++;
+		}
+	}
+	for (int i: digits) {
+		cout << i << " ";
+	}
+}	
