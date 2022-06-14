431d5c762e5f20661a8b3e83b820911ecd515979
Boxuan.Shan <boxuan.shan@gmail.com>
2022-06-15 07:14:45 +0800

    Changed file permisson to 644

--- CC25B/Good_Grass_main/good_grass.cc
+++ CC25B/Good_Grass_main/good_grass.cc
@@ -13,48 +13,48 @@ int main() {
     cin >> a4x >> a4y >> a2x >> a2y;
     cin >> b4x >> b4y >> b2x >> b2y;
     cin >> f4x >> f4y >> f2x >> f2y;
-    a2x += 1000; 
-    a2y += 1000; 
-    a4x += 1000; 
+    a2x += 1000;
+    a2y += 1000;
+    a4x += 1000;
     a4y += 1000;
-    b2x += 1000; 
-    b2y += 1000; 
-    b4x += 1000; 
+    b2x += 1000;
+    b2y += 1000;
+    b4x += 1000;
     b4y += 1000;
-    f2x += 1000; 
-    f2y += 1000; 
-    f4x += 1000; 
+    f2x += 1000;
+    f2y += 1000;
+    f4x += 1000;
     f4y += 1000;
 
     for (int i = 0; i < 2001; i++) {
-      for (int j = 0; j < 2001; j++) {
-        f[i][j] = false;
-      }
+        for (int j = 0; j < 2001; j++) {
+            f[i][j] = false;
+        }
     }
     for (int i = a4x; i < a2x; i++) {
-      for (int j = a4y; j < a2y; j++) {
-        f[i][j] = true;
-      }
+        for (int j = a4y; j < a2y; j++) {
+            f[i][j] = true;
+        }
     }
     for (int i = b4x; i < b2x; i++) {
-      for (int j = b4y; j < b2y; j++) {
-        f[i][j] = true;
-      }
+        for (int j = b4y; j < b2y; j++) {
+            f[i][j] = true;
+        }
     }
     for (int i = f4x; i < f2x; i++) {
-    	for (int j = f4y; j < f2y; j++) {
-        f[i][j] = false;
-      }
+        for (int j = f4y; j < f2y; j++) {
+            f[i][j] = false;
+        }
     }
     for (int i = 0; i < 2001; i++) {
-      for (int j = 0; j < 2001; j++) {
-				if (f[i][j]) {
-					ans++;
-				}
-      }
+        for (int j = 0; j < 2001; j++) {
+            if (f[i][j]) {
+                ans++;
+            }
+        }
     }
 
-		cout << ans;
+    cout << ans;
 
     return 0;
 }
